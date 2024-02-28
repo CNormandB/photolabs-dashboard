f23679db7ebfa66c5a36bf2a943c1f1b18f6162c
Casandra Normand <casandra.normand@outlook.com>
2024-02-28 08:08:20 -0500

    Add Loading + Panel

---
+++ src/components/Panel.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Panel extends Component {
+  render() {
+    const { id, label, value, onSelect } = this.props;
+
+    return (
+      <section onClick={() => onSelect(id)}
+        className="dashboard__panel"
+      >
+        <h1 className="dashboard__panel-header">{label}</h1>
+        <p className="dashboard__panel-value">{value}</p>
+      </section>
+    );
+  }
+}
+
+export default Panel;
